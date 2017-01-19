2e8adf715d90516d0a7c078a97c29b36a84d9641
이준석 <semteo04@naver.om>
2017-01-19 20:12:31 +0900

    gjhhjghckgjhg.k

---
+++ QWeR/src/QWeR.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class QWeR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner aa=new Scanner(System.in);
+		String aaaa;
+		aaaa=aa.next();
+		System.out.println(aaaa);
+	}
+
+}
